f95190332dcde6d625a65178d8ed074f46434d5e
Avinash829 <avinashpappala@gmail.com>
2025-07-31 17:58:54 +0530

    final changes

--- Backend/server.js
+++ Backend/server.js
@@ -15,10 +15,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
+const corsOptions = {
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
-}));
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("Response from API");
